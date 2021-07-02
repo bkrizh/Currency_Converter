c27b22b5b2ece08391dcf834b636b9a99598e9a6
Parag <paragdurafe@outlook.com>
2021-07-02 21:18:01 +0530

    Login

--- home.py
+++ home.py
@@ -1,3 +1,17 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen,ScreenManager
+Builder.load_file('core.ky')
+
+class Login(Screen):
+    pass
+
+class RootWidget(ScreenManager):
+    pass
+
+class Currency(App):
+    def build(self):
+        return RootWidget()
+
+if __name__=="__main__":
+    Currency().run()
